62b2f31bf2745a7be19f5a7b783c826e89dc0e06
rdeusser <rdeusser@salesforce.com>
2019-08-15 12:29:06 -0400

    Fix zookeeper file name

--- script/travis_zk.sh
+++ script/travis_zk.sh
@@ -7,6 +7,6 @@ else
 fi
 
 wget "http://apache.cs.utah.edu/zookeeper/zookeeper-${ZK_VERSION}/apache-zookeeper-${ZK_VERSION}.tar.gz"
-tar -xvf "zookeeper-${ZK_VERSION}.tar.gz"
+tar -xvf "apache-zookeeper-${ZK_VERSION}.tar.gz"
 mv apache-zookeeper-$ZK_VERSION zk
 mv ./zk/conf/zoo_sample.cfg ./zk/conf/zoo.cfg
