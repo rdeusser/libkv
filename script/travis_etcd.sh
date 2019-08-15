4a49214edd888028dc86cdedbf2816ac75bb575a
rdeusser <rdeusser@salesforce.com>
2019-08-15 12:09:06 -0400

    Bump etcd to v3.3.13

--- script/travis_etcd.sh
+++ script/travis_etcd.sh
@@ -3,7 +3,7 @@
 if [  $# -gt 0 ] ; then
     ETCD_VERSION="$1"
 else
-    ETCD_VERSION="3.3.10"
+    ETCD_VERSION="3.3.13"
 fi
 
 curl -L https://github.com/etcd-io/etcd/releases/download/v$ETCD_VERSION/etcd-v$ETCD_VERSION-linux-amd64.tar.gz -o etcd-v$ETCD_VERSION-linux-amd64.tar.gz
