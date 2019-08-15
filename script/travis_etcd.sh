92a8eac22a17665c325daefc88f2c820b46e929f
rdeusser <rdeusser@salesforce.com>
2019-08-15 11:57:43 -0400

    Bump etcd to 3.3.10

--- script/travis_etcd.sh
+++ script/travis_etcd.sh
@@ -3,9 +3,9 @@
 if [  $# -gt 0 ] ; then
     ETCD_VERSION="$1"
 else
-    ETCD_VERSION="2.2.0"
+    ETCD_VERSION="3.3.10"
 fi
 
-curl -L https://github.com/coreos/etcd/releases/download/v$ETCD_VERSION/etcd-v$ETCD_VERSION-linux-amd64.tar.gz -o etcd-v$ETCD_VERSION-linux-amd64.tar.gz
+curl -L https://github.com/etcd-io/etcd/releases/download/v$ETCD_VERSION/etcd-v$ETCD_VERSION-linux-amd64.tar.gz -o etcd-v$ETCD_VERSION-linux-amd64.tar.gz
 tar xzvf etcd-v$ETCD_VERSION-linux-amd64.tar.gz
 mv etcd-v$ETCD_VERSION-linux-amd64 etcd
